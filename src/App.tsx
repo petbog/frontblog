58259219ef03ac5f0558706d69722eaede2f22b4
Petrov Bogdan <bogdan_emdetei_petrov@mail.ru>
2023-09-10 22:44:12 +0300

    страницы регистрации и лог

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom"
 import Home from './pages/Home/Home';
+import Auth from './pages/Auth/Auth';
+import Register from './pages/Register/Register';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/auth' element={<Auth />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
     </div>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,8 @@
+import { Link, useLocation } from 'react-router-dom'
 import s from './Header.module.scss'
 
 const Header = () => {
-
+    const location = useLocation()
     const isAuth = false
 
     return (
@@ -10,15 +11,45 @@ const Header = () => {
                 <h3> Blog </h3>
             </div>
             <div className={s.link}>
-                {
-                    isAuth ? (<>
-                        <button className={s.link__article}>Написать статью</button>
-                        <button className={s.link__exid}>Выйти</button>
-                    </>) : (<>
-                        <div className={s.link__forward}>Войти</div>
-                        <button className={s.link__create}>Создать аккаунт</button>
-                    </>)
-                }
+            {
+  location.pathname === '/' ? (
+    isAuth ? (
+      <>
+        <div className={s.link__article}>Написать статью</div>
+        <button className={s.link__exid}>Выйти</button>
+      </>
+    ) : (
+      <>
+        <Link className={s.innerLinkForward} to='/auth'>
+          <div className={s.link__forward}>Войти</div>
+        </Link>
+        <Link className={s.innerLinkForward} to='/register' >
+        <button className={s.link__create}>Создать аккаунт</button>
+        </Link>
+      </>
+    )
+  ) : (
+    location.pathname === '/register' ? (
+      <>
+        <Link className={s.innerLinkForward} to='/'>
+          <div className={s.link__forward}>На главную</div>
+        </Link>
+        <Link className={s.innerLinkForward} to='/auth'>
+          <div className={s.link__forward}>Войти</div>
+        </Link>
+      </>
+    ) : (
+      <>
+        <Link className={s.innerLinkForward} to='/'>
+          <div className={s.link__forward}>На главную</div>
+        </Link>
+        <Link to='/register'>
+          <button className={s.link__create}>Создать аккаунт</button>
+        </Link>
+      </>
+    )
+  )
+}
 
             </div>
         </div>
